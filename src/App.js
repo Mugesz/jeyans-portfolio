63eebbe8252ffce68557f9ef79c21bea7ea1dc97
frekerzzzz <kannamugesh135@gmail.com>
2025-01-25 22:50:58 +0530

    kll

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Contact from "./Components/Contact/Contact";
 import Ico from "./Components/capstone projects/Ico"
 import Peertopeer from "./Components/capstone projects/Peertopeer"
 import Crypto from "./Components/capstone projects/Crypto";
+import Voltrix from "./Components/capstone projects/Voltrix";
+import Digi from "./Components/capstone projects/digi"
 
 
 function App() {
@@ -22,6 +24,17 @@ function App() {
         <Route path="/capstone/ico" element={<Ico />} />
         <Route path="/capstone/peer-to-peer" element={<Peertopeer />} />
         <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+        <Route path="/capstone/voltrix" element={<Voltrix />} />
+        <Route path="/capstone/digi" element={<Digi />} />
+        <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+        <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+        <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+        <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+        <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+        <Route path="/capstone/Crypto-hub" element={<Crypto />} />
+
+
+        
       </Routes>
     </BrowserRouter>
   );
