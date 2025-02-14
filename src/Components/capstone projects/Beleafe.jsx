26084f4287daee77cf3530fe41b7779a972bb91f
mugesz <kannamugesh135@gmail.com>
2025-02-14 12:42:46 +0530

    beleaf s

--- src/Components/capstone projects/Beleafe.jsx
+++ src/Components/capstone projects/Beleafe.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import "./style.css";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import peertop from "../../Asserts/peertop.svg";
-import peerfull from "../../Asserts/peerfull.png";
-import Tcypro from "../../Asserts/tcypro.svg";
-import Tph from "../../Asserts/Tph.svg";
-import Tdigi from "../../Asserts/Tdigi.png";
+import beleaftop from "../../Asserts/Beleaf-top.png";
+import Beleaft from "../../Asserts/Beleaf-t.png";
+import Rifs from "../../Asserts/Rifaw-small.png";
+import Rifaw from "../../Asserts/Rifaw-small.png";
+import Brand from "../../Asserts/BrandI-small.png";
 import { Link } from "react-router-dom";
 
 const Beleafe = () => {
@@ -21,30 +21,26 @@ const Beleafe = () => {
         <div className="d-flex justify-content-center align-items-baseline ">
           <div className="contents">
             <h3 className="ico mt-3 w-75">
-              Peer to Peer (P2P) Landing page revamp design{" "}
-            </h3>
+            Beleaf
+            Flash loan arbitrage bot (white paper)              </h3>
             <div className="d-flex w-50 justify-content-between web mt-5">
-              <h4>Website Design</h4>
+              <h4>Pitch Deck Design</h4>
               <h4>2024</h4>
             </div>
             <p className="initial mt-5 w-75">
-              Peer-to-Peer (P2P) is a decentralized network model where users
-              interact directly with each other to exchange goods or services,
-              such as cryptocurrencies, without the need for an intermediary
-              or central authority. Each participant acts as both a buyer and
-              a seller in the transaction.
+            This is one of the flash loan arbitrage bot whitepaper design for beleaf technology.
             </p>
           </div>
 
           <div className="">
-            <img className="icoimg" src={peertop} alt="" />
+            <img className="icoimg" src={beleaftop} alt="" />
           </div>
         </div>
       </div>
       <div className="main-caps container">
         <h3 className="view">Landing Page View</h3>
         <div className="full-image">
-          <img className="ico-full" src={peerfull} alt="" />
+          <img className="ico-full" src={Beleaft} alt="" />
         </div>
       </div>
 
@@ -59,32 +55,36 @@ const Beleafe = () => {
           <div className="pt-3 pb-3 some">
             <div className="content-div w-100 d-flex justify-content-center align-items-center gap-5 mb-2"></div>
             <div className=" d-grid  last last-card ">
-              <div className="inline-card pt-4 pb-2 blue-1">
-                <div className="blue-wrapper">
-                  <img src={Tcypro} alt="" />
-                </div>
+            <Link to="/capstone/Rifa-World-Company">
+                  <div className="inline-card pt-4 pb-2 blue-2">
+                    <div className="blue-wrapper">
+                      <img src={Rifs} alt="" />
+                    </div>
 
-                <h3 className="ico">Crypto Hub</h3>
-                <p className="para">Website Design</p>
-              </div>
+                    <h3 className="ico">Rifa World Company</h3>
+                    <p className="para">Banner Design</p>
+                  </div>
+                </Link>
+                <Link to="/capstone/Rifa-World-Company-2">
+                  <div className="inline-card pt-4 pb-2 blue-2">
+                    <div className="blue-wrapper">
+                      <img src={Rifaw} alt="" />
+                    </div>
 
-              <div className="inline-card pt-4 pb-2 blue-2">
-                <div className="blue-wrapper">
-                  <img src={Tph} alt="" />
-                </div>
+                    <h3 className="ico">Rifa World Company 2</h3>
+                    <p className="para">Banner Design</p>
+                  </div>
+                </Link>{" "}
+                <Link to="/capstone/ico">
+                  <div className="inline-card pt-4 pb-2 blue-2">
+                    <div className="blue-wrapper">
+                      <img src={Brand} alt="" />
+                    </div>
 
-                <h3 className="ico">PH Online Ventures</h3>
-                <p className="para">Website Design</p>
-              </div>
-
-              <div className="inline-card pt-4 pb-2 blue-2">
-                <div className="blue-wrapper">
-                  <img src={Tdigi} alt="" />
-                </div>
-
-                <h3 className="ico">Digi Class</h3>
-                <p className="para">Website Design</p>
-              </div>
+                    <h3 className="ico">Brand Identity</h3>
+                    <p className="para">Graphic Design</p>
+                  </div>
+                </Link>{" "}
             </div>
           </div>
         </div>
